46b440d2afab714ebdb36a08815a62b28da25b23
Larry6oy <peng.larry@outlook.com>
2020-12-28 17:04:07 -0500

    Hope you enjoy this!

--- math_test.py
+++ math_test.py
@@ -1,16 +1,30 @@
 import random
 
 lives = 3
+score = 0
+
+heart_symbol = u'\u2764 '
 
 for conter in range(1, 11):
-
+    print(heart_symbol * lives)
     left = random.randrange(1,9);
     right = random.randrange(1,9);
 
     res = input( str(left) + 'x' + str(right) + '=');
     if( int(res) == left * right):
-        print(res, ', correct!');
+        print(res, 'is correct!');
+        score = score + 1;
     else:
-        print(res, ',Oops!');
+        print(res, 'is incorrect!');
+        lives = lives - 1
+        if (lives == 0):
+            break
+
+
+if(lives == 0):
+    print('You are out of lives! You lose!')
+else:
+    print('Congratulations! You finished! Your score is ' + str(score) + '/10');
+
 # eqations = ['3x3=', '4x5=', '3x7', '9x9=', '10x10=', '7x7=', '8x8=']
 #chalange = random(res)
