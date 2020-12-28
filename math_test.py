dd53ce76ea9dd4866231d1922c605d358ee5d7e1
Larry6oy <peng.larry@outlook.com>
2020-12-28 16:28:52 -0500

    First virshtion of math_test.py

--- math_test.py
+++ math_test.py
@@ -1,6 +1,16 @@
 import random
 
-
 lives = 3
 
-eqations = ['3x3=', '']
+for conter in range(1, 11):
+
+    left = random.randrange(1,9);
+    right = random.randrange(1,9);
+
+    res = input( str(left) + 'x' + str(right) + '=');
+    if( int(res) == left * right):
+        print(res, ', correct!');
+    else:
+        print(res, ',Oops!');
+# eqations = ['3x3=', '4x5=', '3x7', '9x9=', '10x10=', '7x7=', '8x8=']
+#chalange = random(res)
